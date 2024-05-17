f8efe38c354967913d1db41eb8f9f33bc7d506d1
Juan José Navarro Morales <juanjosenavarromorales@gmail.com>
2024-05-17 19:44:57 +0200

    persist favorites in browser local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './ui/core/nav/nav.component';
+import { JobsDataService } from './services/store/jobs-data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-job-search';
+
+  jobsDataService = inject(JobsDataService);
+  readonly JOBS_ID_STORAGE_KEY = 'jobsIdFavorites';
+
+  ngOnInit() {
+    this.getLocatStorageData();
+  }
+
+  getLocatStorageData() {
+    const items = localStorage.getItem(this.JOBS_ID_STORAGE_KEY);
+    if (items && Array.isArray(JSON.parse(items))) {
+      this.jobsDataService.setFavoritesIds(JSON.parse(items));
+    }
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  handleBeforeUnload(_event: any) {
+    this.jobsDataService
+      .getFavoritesIds()
+      .subscribe((jobsIdFavorites: string[] | null) => {
+        localStorage.setItem(
+          this.JOBS_ID_STORAGE_KEY,
+          JSON.stringify(jobsIdFavorites)
+        );
+      });
+  }
 }
